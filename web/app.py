356808cf53d99d1028405fc8653c8a57cc88b915
Berat Bayraktar <sakaryaliberatbey@proton.me>
2025-03-22 16:32:49 +0300

    fix

--- web/app.py
+++ web/app.py
@@ -1,30 +1,18 @@
-import threading
+from flask import Flask, render_template
 
-def server():
-    from flask import Flask, render_template
+app = Flask(__name__)
 
-    app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+@app.route('/try')
+def try_page():
+    return render_template('try.html')
 
-    @app.route('/info')
-    def info():
-        return render_template('info.html')
+@app.route('/info')
+def info():
+    return render_template('info.html')
 
-    @app.route('/try')
-    def try_page():
-        return render_template('try.html')
-
-    if __name__ == '__main__':
-        app.run(debug=True)
-
-serverThread = threading.Thread(target=server)
-serverThread.start()
-
-def main():
-    print("Hello World!")
-
-mainThread = threading.Thread(target=main)
-mainThread.start()
+if __name__ == '__main__':
+    app.run(debug=True)
